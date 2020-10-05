e98bdd9ccfe211d7fadead2976dd0d349f56fcac
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2020-10-05 17:30:27 +0100

    Added movement scripts. Basic scenes.

---
+++ CC3_PROJECT/Assets/Scripts/Player_Look.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Look : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxis Axis = RotationAxis.MouseX;
+
+    public float MinimumVertical = -45.0f;
+    public float MaximumVertical = 45.0f;
+
+    public float HorizonitalSensitivity = 3.0f;
+    public float VerticalSensitivity = 3.0f;
+    
+    private float _rotationX = 0.0f;
+
+    void Update()
+    {
+        //For X
+        if(Axis == RotationAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * HorizonitalSensitivity, 0);
+        } 
+        //For Y
+        else if (Axis == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * VerticalSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, MinimumVertical, MaximumVertical);
+
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
---
+++ CC3_PROJECT/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    public float DownForce = -10.0f;
+
+    private CharacterController _playerController;
+
+    void Start()
+    {
+        _playerController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Speed;
+        float deltaZ = Input.GetAxis("Vertical") * Speed;
+        Vector3 pMove = new Vector3(deltaX, 0, deltaZ);
+        pMove = Vector3.ClampMagnitude(pMove, Speed);
+        pMove.y = DownForce; //Gravity
+        pMove *= Time.deltaTime;
+        pMove = transform.TransformDirection(pMove);
+        _playerController.Move(pMove);
+    }
+}
