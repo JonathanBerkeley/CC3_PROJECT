1f8880004e76d75d8294d8d5568927124b3398b7
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2020-10-09 23:02:16 +0100

    Fixed issue with velocity not resetting when grounded. Began coding jump logic.

--- CC3_PROJECT/Assets/Scripts/Player_Move.cs
+++ CC3_PROJECT/Assets/Scripts/Player_Move.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
+    [Range(0.0f, 100.0f)]
     public float speed = 5.0f;
     public float speedShiftMultiplier = 500.0f;
+    [Range(0.0f, 100.0f)]
+    public float jumpPower = 4.0f;
     public float downForce = -10.0f;
     public bool accelerateDownForce = false;
 
     private float velocityY = 0.0f;
-
+    private float baseVelocityY;
+    private float deltaY = 0.0f;
     private CharacterController _playerController;
 
     void Start()
     {
+        baseVelocityY = velocityY;
         _playerController = GetComponent<CharacterController>();
     }
 
@@ -22,10 +27,24 @@ public class Player_Move : MonoBehaviour
     {
         float deltaX = Input.GetAxisRaw("Horizontal") * (speed * (Input.GetButtonDown("Shift") ? speedShiftMultiplier : 1));
         float deltaZ = Input.GetAxisRaw("Vertical") * (speed * (Input.GetButtonDown("Shift") ? speedShiftMultiplier : 1));
-        Vector3 pMove = new Vector3(deltaX, 0.0f, deltaZ);
-
         if (accelerateDownForce && !_playerController.isGrounded)
             velocityY += downForce * Time.deltaTime;
+        else if (accelerateDownForce && _playerController.isGrounded)
+            velocityY = 0.0f; //Reset downforce on land
+        /* WIP
+        if (_playerController.isGrounded && Input.GetButton("Jump"))
+        {
+            deltaY += jumpPower;
+            Debug.Log(deltaY);
+        } 
+        else
+        {
+            deltaY = 0.0f;
+        }
+        */
+        Vector3 pMove = new Vector3(deltaX, deltaY, deltaZ);
+
+        
         pMove.y = downForce + velocityY; //Gravity
         pMove *= Time.deltaTime;
         pMove = transform.TransformDirection(pMove);
