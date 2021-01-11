d6fd32107b9c96082572b8f641ea498a9b0b6ab6
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-01-11 07:36:03 +0000

    Sync changes (StaticInput class etc) to remote

---
+++ CC3_PROJECT/Assets/Scripts/PlayerScripts/GrapplingHook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code from following tutorial https://www.youtube.com/watch?v=Xgh4v1w5DxU
+//SRC: Dani Devi
+public class GrapplingHook : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Vector3 grapplePosition;
+    
+    public LayerMask grapplableSurface;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
